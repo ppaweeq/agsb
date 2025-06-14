a41188bc6893304ab5b962f32dbfe4499a70e8c5
ppaweeq <yyitti062@gmail.com>
2025-06-14 20:55:25 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -35,7 +35,7 @@ USER_NAME = "kkddytd"         # 用户名
 UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
 PORT = 49999                   # Vmess端口，留空或0则自动生成
 DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+CF_TOKEN = "eyJhIjoiZWY0MjRmOTg5ZTM5NTNjNTE0MTk0YjZiMjcxNjQ4NTEiLCJ0IjoiZGFjYTUxNTctODkxYy00MzI4LWIxYjctMmUxOGYyNThmOWMzIiwicyI6IlpHUXdOamMxT1RrdE5UTmxOUzAwT0RWaExXSXhOMk10TmpObE56UTRPRFZsWkdZdyJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
